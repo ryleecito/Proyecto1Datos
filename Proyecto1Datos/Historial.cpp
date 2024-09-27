4957c8497b15e0ed95ef4556c6f21908cd556193
ryleecito <manuel.dealmada.rodriguez@est.una.ac.cr>
2024-09-27 17:50:35 -0600

    probando

--- Proyecto1Datos/Historial.cpp
+++ Proyecto1Datos/Historial.cpp
@@ -185,11 +185,9 @@ std::string Historial::getFiltro()
 void Historial::setFiltro(std::string filtro)
 {
 	this->filtro = filtro;
-	moverseAPrimeraCoincidencia();
+	
 }
 
-// Este metodo lo utilizamos para que cuando agreguemos un filtro,
-// el historial se mueva a la primera coincidencia que encuentre
 void Historial::moverseAPrimeraCoincidencia()
 {
 	if (filtro.empty()) {
